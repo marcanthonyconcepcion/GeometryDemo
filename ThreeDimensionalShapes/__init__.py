1cd2acfd6158bd8cd9c065dfac2c3cacaccc1a5e
Marc Concepcion <marcanthonyconcepcion@gmail.com>
2021-03-28 01:55:47 +0800

    Add code headers

--- ThreeDimensionalShapes/__init__.py
+++ ThreeDimensionalShapes/__init__.py
@@ -1,3 +1,10 @@
+__author__ = "Marc Concepcion"
+__copyright__ = "Copyright 2021, Marc Concepcion"
+__credits__ = ["Marc Concepcion"]
+__maintainer__ = "Marc Concepcion"
+__email__ = "marcanthonyconcepcion@gmail.com"
+__status__ = "Demo"
+
 from ThreeDimensionalShapes.prism import Prism
 from ThreeDimensionalShapes.rectangular_prism import RectangularPrism
 from ThreeDimensionalShapes.triangular_prism import TriangularPrism
